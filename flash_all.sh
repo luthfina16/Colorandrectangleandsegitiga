9fedcddc4cfc5f48334c0252ffea6f9ddee36bad
Luthfi Nur Ariri <160535600+luthfina16@users.noreply.github.com>
2025-04-24 13:02:40 +0700

    Add files via upload

---
+++ flash_all.sh
@@ -0,0 +1,74 @@
+fastboot $* getvar product 2>&1 | grep "^product: *ysl$"
+if [ $? -ne 0 ] ; then echo "Missmatching image and device"; exit 1; fi
+fastboot $* flash tz `dirname $0`/images/tz.mbn
+if [ $? -ne 0 ] ; then echo "Flash tz error"; exit 1; fi
+fastboot $* flash sbl1 `dirname $0`/images/sbl1.mbn
+if [ $? -ne 0 ] ; then echo "Flash sbl1 error"; exit 1; fi
+fastboot $* flash rpm `dirname $0`/images/rpm.mbn
+if [ $? -ne 0 ] ; then echo "Flash rpm error"; exit 1; fi
+fastboot $* flash aboot `dirname $0`/images/emmc_appsboot.mbn
+if [ $? -ne 0 ] ; then echo "Flash aboot error"; exit 1; fi
+fastboot $* flash devcfg `dirname $0`/images/devcfg.mbn
+if [ $? -ne 0 ] ; then echo "Flash devcfg error"; exit 1; fi
+fastboot $* flash keymaster `dirname $0`/images/keymaster64.mbn
+if [ $? -ne 0 ] ; then echo "Flash keymaster error"; exit 1; fi
+fastboot $* flash cmnlib `dirname $0`/images/cmnlib_30.mbn
+if [ $? -ne 0 ] ; then echo "Flash cmnlib error"; exit 1; fi
+fastboot $* flash cmnlib64 `dirname $0`/images/cmnlib64_30.mbn
+if [ $? -ne 0 ] ; then echo "Flash cmnlib64 error"; exit 1; fi
+fastboot $* flash lksecapp `dirname $0`/images/lksecapp.mbn
+if [ $? -ne 0 ] ; then echo "Flash lksecapp error"; exit 1; fi
+fastboot $* flash tzbak `dirname $0`/images/tz.mbn
+if [ $? -ne 0 ] ; then echo "Flash tzbak error"; exit 1; fi
+fastboot $* flash sbl1bak `dirname $0`/images/sbl1.mbn
+if [ $? -ne 0 ] ; then echo "Flash sbl1bak error"; exit 1; fi
+fastboot $* flash rpmbak `dirname $0`/images/rpm.mbn
+if [ $? -ne 0 ] ; then echo "Flash rpmbak error"; exit 1; fi
+fastboot $* flash abootbak `dirname $0`/images/emmc_appsboot.mbn
+if [ $? -ne 0 ] ; then echo "Flash abootbak error"; exit 1; fi
+fastboot $* flash devcfgbak `dirname $0`/images/devcfg.mbn
+if [ $? -ne 0 ] ; then echo "Flash devcfgbak error"; exit 1; fi
+fastboot $* flash keymasterbak `dirname $0`/images/keymaster64.mbn
+if [ $? -ne 0 ] ; then echo "Flash keymasterbak error"; exit 1; fi
+fastboot $* flash cmnlibbak `dirname $0`/images/cmnlib_30.mbn
+if [ $? -ne 0 ] ; then echo "Flash cmnlibbak error"; exit 1; fi
+fastboot $* flash cmnlib64bak `dirname $0`/images/cmnlib64_30.mbn
+if [ $? -ne 0 ] ; then echo "Flash cmnlib64bak error"; exit 1; fi
+fastboot $* flash lksecappbak `dirname $0`/images/lksecapp.mbn
+if [ $? -ne 0 ] ; then echo "Flash lksecappbak error"; exit 1; fi
+
+fastboot $* erase boot
+if [ $? -ne 0 ] ; then echo "erase boot error"; exit 1; fi
+
+fastboot $* flash modem `dirname $0`/images/NON-HLOS.bin
+if [ $? -ne 0 ] ; then echo "Flash modem error"; exit 1; fi
+fastboot $* flash system `dirname $0`/images/system.img
+if [ $? -ne 0 ] ; then echo "Flash system error"; exit 1; fi
+fastboot $* flash vendor `dirname $0`/images/vendor.img
+if [ $? -ne 0 ] ; then echo "Flash vendor error"; exit 1; fi
+fastboot $* flash cache `dirname $0`/images/cache.img
+if [ $? -ne 0 ] ; then echo "Flash cache error"; exit 1; fi
+fastboot $* flash userdata `dirname $0`/images/userdata.img
+if [ $? -ne 0 ] ; then echo "Flash userdata error"; exit 1; fi
+fastboot $* flash recovery `dirname $0`/images/recovery.img
+if [ $? -ne 0 ] ; then echo "Flash recovery error"; exit 1; fi
+fastboot $* flash boot `dirname $0`/images/boot.img
+if [ $? -ne 0 ] ; then echo "Flash boot error"; exit 1; fi
+fastboot $* flash dsp `dirname $0`/images/adspso.bin
+if [ $? -ne 0 ] ; then echo "Flash dsp error"; exit 1; fi
+fastboot $* flash mdtp `dirname $0`/images/mdtp.img
+if [ $? -ne 0 ] ; then echo "Flash mdtp error"; exit 1; fi
+fastboot $* erase splash
+if [ $? -ne 0 ] ; then echo "erase splash error"; exit 1; fi
+fastboot $* flash splash `dirname $0`/images/splash.img
+if [ $? -ne 0 ] ; then echo "Flash splash error"; exit 1; fi
+fastboot $* erase DDR
+if [ $? -ne 0 ] ; then echo "erase DDR error"; exit 1; fi
+fastboot $* flash cust `dirname $0`/images/cust.img
+if [ $? -ne 0 ] ; then echo "Flash cust error"; exit 1; fi
+#fastboot $* erase config
+
+fastboot $* erase misc
+if [ $? -ne 0 ] ; then echo "erase misc error"; exit 1; fi
+fastboot $* reboot
+if [ $? -ne 0 ] ; then echo "reboot error"; exit 1; fi
---
+++ flash_all_lock.bat
@@ -0,0 +1,46 @@
+fastboot %* getvar product 2>&1 | findstr /r /c:"^product: *ysl" || echo Missmatching image and device
+fastboot %* getvar product 2>&1 | findstr /r /c:"^product: *ysl" || exit /B 1
+fastboot %*  flash crclist %~dp0images\crclist.txt || @echo "Flash crclist error" && exit /B 1
+fastboot %*  flash sparsecrclist %~dp0images\sparsecrclist.txt || @echo "Flash sparsecrclist error" && exit /B 1
+fastboot %* flash tz %~dp0images\tz.mbn || @echo "Flash tz error" && exit /B 1
+fastboot %* flash sbl1 %~dp0images\sbl1.mbn || @echo "Flash sbl1 error" && exit /B 1
+fastboot %* flash rpm %~dp0images\rpm.mbn || @echo "Flash rpm error" && exit /B 1
+fastboot %* flash aboot %~dp0images\emmc_appsboot.mbn || @echo "Flash emmc_appsboot error" && exit /B 1
+fastboot %* flash devcfg %~dp0images\devcfg.mbn || @echo "Flash devcfg error" && exit /B 1
+fastboot %* flash keymaster %~dp0images\keymaster64.mbn || @echo "Flash keymaster error" && exit /B 1
+fastboot %* flash cmnlib %~dp0images\cmnlib_30.mbn || @echo "Flash cmnlib error" && exit /B 1
+fastboot %* flash cmnlib64 %~dp0images\cmnlib64_30.mbn || @echo "Flash cmnlib64 error" && exit /B 1
+fastboot %* flash lksecapp %~dp0images\lksecapp.mbn || @echo "Flash lksecapp error" && exit /B 1
+
+fastboot %* flash tzbak %~dp0images\tz.mbn || @echo "Flash tzbak error" && exit /B 1
+fastboot %* flash sbl1bak %~dp0images\sbl1.mbn || @echo "Flash sbl1bak error" && exit /B 1
+fastboot %* flash rpmbak %~dp0images\rpm.mbn || @echo "Flash uboot rpmbak" && exit /B 1
+fastboot %* flash abootbak %~dp0images\emmc_appsboot.mbn || @echo "Flash abootbak error" && exit /B 1
+fastboot %* flash devcfgbak %~dp0images\devcfg.mbn || @echo "Flash devcfgbak error" && exit /B 1
+fastboot %* flash keymasterbak %~dp0images\keymaster64.mbn || @echo "Flash keymasterbak error" && exit /B 1
+fastboot %* flash cmnlibbak %~dp0images\cmnlib_30.mbn || @echo "Flash cmnlibbak error" && exit /B 1
+fastboot %* flash cmnlib64bak %~dp0images\cmnlib64_30.mbn || @echo "Flash cmnlib64bak error" && exit /B 1
+fastboot %* flash lksecappbak %~dp0images\lksecapp.mbn || @echo "Flash lksecappbak error" && exit /B 1
+
+fastboot %* erase boot
+fastboot %* flash modem %~dp0images\NON-HLOS.bin || @echo "Flash modem error" && exit /B 1
+fastboot %* flash system %~dp0images\system.img || @echo "Flash system error" && exit /B 1
+fastboot %* flash vendor %~dp0images\vendor.img || @echo "Flash vendor error" && exit /B 1
+fastboot %* flash cache %~dp0images\cache.img || @echo "Flash cache error" && exit /B 1
+fastboot %* flash userdata %~dp0images\userdata.img || @echo "Flash userdata error" && exit /B 1
+fastboot %* flash recovery %~dp0images\recovery.img || @echo "Flash recovery error" && exit /B 1
+fastboot %* flash boot %~dp0images\boot.img || @echo "Flash boot error" && exit /B 1
+
+fastboot %* flash dsp %~dp0images\adspso.bin || @echo "Flash adspso error" && exit /B 1
+fastboot %* flash mdtp %~dp0images\mdtp.img || @echo "Flash mdtp error" && exit /B 1
+fastboot %* erase splash
+fastboot %* flash splash %~dp0images\splash.img || @echo "Flash splash error" && exit /B 1
+fastboot %* erase DDR
+
+fastboot %* flash cust %~dp0images\cust.img || @echo "Flash cust error" && exit /B 1
+
+fastboot %* erase misc
+
+fastboot %* oem lock
+fastboot %* reboot
+pause
